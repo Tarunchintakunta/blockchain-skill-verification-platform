c1f18c2a80fb54a4708ec6c7a2929347303a8284
Tarun Chintakunta <tarunchintakunta@gmail.com>
2026-02-18 17:34:43 +0530

    db: add lazy-initialized Neon PostgreSQL connection

---
+++ src/db/index.ts
@@ -0,0 +1,20 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
+
+const getDb = () => {
+  const sql = neon(process.env.DATABASE_URL!);
+  return drizzle(sql, { schema });
+};
+
+let _db: ReturnType<typeof getDb> | null = null;
+
+export const db = new Proxy({} as ReturnType<typeof getDb>, {
+  get(_, prop) {
+    if (!_db) {
+      _db = getDb();
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return (_db as any)[prop];
+  },
+});
