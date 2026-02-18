73b4eb69f779a1fde56c6412314e7f9663a87e1f
Tarun Chintakunta <tarunchintakunta@gmail.com>
2026-02-18 17:34:45 +0530

    contracts: define Credential struct and state variables

--- contracts/SkillCredential.sol
+++ contracts/SkillCredential.sol
@@ -38,3 +38,33 @@ contract SkillCredential is ERC721, ERC721URIStorage, AccessControl {
     event CredentialRevoked(uint256 indexed tokenId);
 
     constructor() ERC721("SkillCredential", "SKLC") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(ISSUER_ROLE, msg.sender);
+    }
+
+    /**
+     * @dev Issue a new credential NFT to a holder.
+     * @param holder Address receiving the credential
+     * @param credentialHash Keccak256 hash of credential data
+     * @param metadataURI IPFS URI for credential metadata
+     */
+    function issueCredential(
+        address holder,
+        string calldata credentialHash,
+        string calldata metadataURI
+    ) external onlyRole(ISSUER_ROLE) returns (uint256) {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(holder, tokenId);
+        _setTokenURI(tokenId, metadataURI);
+
+        _credentials[tokenId] = Credential({
+            holder: holder,
+            issuer: msg.sender,
+            credentialHash: credentialHash,
+            issuedAt: block.timestamp,
+            isValid: true
+        });
+
+        _holderCredentials[holder].push(tokenId);
+        _issuerCredentials[msg.sender].push(tokenId);
+
